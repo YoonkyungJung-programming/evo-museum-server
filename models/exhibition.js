25027d26297c119744e3a43b27fb4b2afeea660b
YoonkyungJung <yoonkyung0309@gmail.com>
2022-07-26 16:09:52 +0900

    sqlite 데이터베이스 연결

---
+++ models/exhibition.js
@@ -0,0 +1,26 @@
+module.exports = function (sequelize, DataTypes) {
+  const exhibition = sequelize.define("Exhibition", {
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    exp: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    exp2: {
+      type: DataTypes.STRING(300),
+      allowNull: true,
+    },
+    imageUrl: {
+      type: DataTypes.STRING(500),
+      allowNull: false,
+    },
+    imageUrl2: {
+      type: DataTypes.STRING(500),
+      allowNull: false,
+    },
+  });
+
+  return exhibition;
+};
--- server.js
+++ server.js
@@ -1,45 +1,99 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const models = require("./models");
+const exhibition = require("./models/exhibition");
 const port = 8080;
 
 app.use(express.json()); //express에 대한 설정
 app.use(cors()); //이제 모든 브라우저에서 가능
 
+//전체 전시 get
 app.get("/exhibitions", (req, res) => {
-  res.send({
-    exhibitions: [
-      {
-        id: 1,
-        name: "The Art of Banksy Online",
-        exp: "아트 오브 뱅크시 온라인",
-        //main page image
-        imageUrl:
-          "https://media.timeout.com/images/105611607/750/422/image.jpg",
-      },
-    ],
-  });
+  models.Exhibition.findAll({
+    //{order : [['createdAt','DESC']],} findAll 안에 넣으면 정렬가능
+    attributes: ["id", "name", "exp", "imageUrl", "createdAt"],
+  })
+    .then((result) => {
+      console.log("EXHIBITIONS :", result);
+      res.send({
+        exhibitions: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("ERROR : 전체 전시 조회에 에러가 발생 ");
+    });
 });
 
-app.get("/exhibitions/1", (req, res) => {
-  res.send({
-    exhibition: [
-      {
-        id: 1,
-        name: "The Art of Banksy Online",
-        exp: "exhibition contents needed !",
-        //Exhibition page image
-        imageUrl2:
-          "https://post-phinf.pstatic.net/MjAxOTEwMjhfNDAg/MDAxNTcyMjQyMzU4ODA2.KUMHlcBR4RinjJov0AavjphuoKwpCdtTV4IxuirH3KMg.qYI-wyqe3fOOYczsVn30XI3OyAK5aJkR4wei1u6JVoEg.JPEG/banksy_exhibition.jpg?type=w1200",
-      },
-    ],
-  });
+//개별전시 :id  get
+app.get("/exhibitions/:id", (req, res) => {
+  const params = req.params;
+  const { id } = params;
+
+  models.Exhibition.findOne({
+    where: {
+      id: id, //id가 일치하는 것 하나만 find 해라
+    },
+    attributes: ["id", "name", "exp2", "imageUrl2", "createdAt"],
+  })
+    .then((result) => {
+      console.log("EXHIBITION : ", exhibition);
+      res.send({
+        exhibition: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("개별 전시 조회에 에러가 발생");
+    });
 });
 
+//전체 전시 post
 app.post("/exhibitions", (req, res) => {
-  res.send("등록되었습니다");
+  const body = req.body;
+  //
+  //Database
+  const { name, exp, exp2, imageUrl, imageUrl2 } = body;
+
+  models.Exhibition.create({
+    name,
+    exp,
+    exp2,
+    imageUrl,
+    imageUrl2,
+  })
+    .then((result) => {
+      console.log("생성 결과 : ", result);
+      res.send({
+        result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send("데이터베이스 Data Create에 에러가 발생");
+    });
+
+  res.send({
+    body,
+  });
 });
 
+//listen
 app.listen(port, () => {
-  console.log("evo museum server on!");
+  console.log("EVO-Museum Server ON : 서버 연결");
+
+  //데이터베이스 동기화
+  models.sequelize
+    .sync()
+    .then(() => {
+      console.log("Database Connected : 데이터베이스 연결 ");
+    })
+    .catch((err) => {
+      console.errror(err);
+      console.log(
+        "Database Connection Error : 데이터베이스 연결에 에러가 발생"
+      );
+      process.exit();
+    });
 });
